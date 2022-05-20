4603e49df0ea21fdfcef986fa5287ce8096755b0
matt-bada <mateusbalda89@gmail.com>
2022-05-19 21:30:14 -0300

    Veiculo e Categoria

---
+++ T1/Entidades/Categoria.java
@@ -0,0 +1,22 @@
+package Entidades;
+
+public class Categoria {
+    private String nome;
+    private int id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ T1/Entidades/Veiculo.java
@@ -0,0 +1,58 @@
+package Entidades;
+
+public class Veiculo {
+    private String placa;
+    private String modelo;
+    private String marca;
+    private int ano;
+    private int potencia;
+    private int numDeLugares;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public int getNumDeLugares() {
+        return numDeLugares;
+    }
+
+    public void setNumDeLugares(int numDeLugares) {
+        this.numDeLugares = numDeLugares;
+    }
+}
